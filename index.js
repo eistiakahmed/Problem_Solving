1c06da5323ca4dce692ffb891cc33b8e57c71968
Eistiak Ahmed <eistiakahmedmeraj@gmail.com>
2026-01-08 11:41:56 +0600

    Problem 3 solve

--- index.js
+++ index.js
@@ -22,8 +22,21 @@ function countVowel(str) {
     }
   }
   return count;
-
 }
 
 // console.log(countVowel('programming'));
 
+// Problem 3: Check for Palindrome
+function isPalindrome(str) {
+  let left = 0;
+  let right = str.length - 1;
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
+// console.log(isPalindrome('hello'));
