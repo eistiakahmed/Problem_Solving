05511aef2ff23e8eeec3c61ee4263bacf0045bdf
Eistiak Ahmed <eistiakahmedmeraj@gmail.com>
2026-01-08 11:09:41 +0600

    First problem solve

---
+++ index.js
@@ -0,0 +1,10 @@
+// Problem 1: Reverse a String
+function reverseString(str){
+  let newStr = ""
+  for(let i = str.length - 1; i>=0; i--){
+    newStr += str[i]
+  }
+  return newStr;
+}
+
+console.log(reverseString("Hello"))
