0c6a84e63d2610c9ac5ea229f6b81d8dabd92dbd
Eistiak Ahmed <eistiakahmedmeraj@gmail.com>
2026-01-08 15:17:18 +0600

    Problem 4 solve

--- index.js
+++ index.js
@@ -40,3 +40,20 @@ function isPalindrome(str) {
   return true;
 }
 // console.log(isPalindrome('hello'));
+
+// Problem 4: Find the Maximum Number
+
+const maxNum = (arr) => {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return max;
+};
+
+// console.log(maxNum([5, 1, 9, 3]));
+
+
